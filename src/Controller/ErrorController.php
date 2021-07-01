75f1966800fd0d8921daa8276f4b8e6b492f5efb
Stève Caillault <steve.caillault@gmail.com>
2021-07-01 12:17:29 +0200

    Ajout du contrôleur appelé en cas d'erreur.

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Contrôleur des pages d'erreur
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\{ 
+    Request, 
+    JsonResponse, 
+    Response 
+};
+use Symfony\Component\Routing\Annotation\Route as RouteAnnotation;
+use App\SiteService;
+
+class ErrorController extends AbstractController
+{
+    /**
+     * Code d'erreur à afficher
+     * @var int
+     */
+    private int $displayingStatusCode = 500;
+
+    /**
+     * Page d'erreur
+     * @param Request $request
+     * @param \Throwable $exception
+     * @param SiteService $siteService
+     * @return Response
+     */
+    #[
+        RouteAnnotation(
+            path: '/error', 
+            name: 'error',
+            methods: [ "GET" ]
+        )
+    ]
+    public function index(
+        Request $request, 
+        \Throwable $exception,
+        SiteService $siteService
+    ) : Response
+    {
+
+
+        $siteService->setWithError(true);
+
+        $statusCode = (method_exists($exception, 'getStatusCode')) ? $exception->getStatusCode() : $exception->getCode();
+        // $errorMessage = $exception->getMessage();
+
+        $allowedCodes = [ 401, 403, 404, 500, ];
+        $this->displayingStatusCode = $statusCode;
+		if(! in_array($this->displayingStatusCode, $allowedCodes))
+		{
+			$this->displayingStatusCode = 500;
+		}
+		
+        $displayingMessage = match($statusCode) {
+            401 => 'Vous devez être identifié pour accéder à cette page.',
+            403 => 'Vous n\'êtes pas autorisé à accéder à cette page.',
+            404 => 'Cette page n\'existe pas ou a été déplacé.',
+            default => 'Une erreur s\'est produite.',
+        };
+
+        // $displayingMessage = $errorMessage;
+
+        $displayingData = [
+            'code' => $this->displayingStatusCode,
+            'message' => $displayingMessage,
+        ];
+
+        if($request->isXmlHttpRequest())
+        {
+            return new JsonResponse([
+                'status' => \App\Controller\AjaxController::STATUS_ERROR,
+                'data' => $displayingData,
+            ], $statusCode);
+        }
+
+        $content = $this->renderView('layout/error.html.twig', $displayingData);
+        $response = new Response($content, $statusCode);
+
+        return $response;
+    }
+
+}
